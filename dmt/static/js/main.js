e3777c5e846bfb5c7169835b0fb74600db29b022
Dmitrii Zheleztsov <soil7800@gmail.com>
2020-12-11 16:27:51 +0300

    Реорганизация католога статических файлов, рефакторинг шаблонов и стилей

--- dmt/static/js/main.js
+++ dmt/static/js/main.js
@@ -1,206 +1,180 @@
-var initPhotoSwipeFromDOM = function(gallerySelector) {
-
-    // parse slide data (url, title, size ...) from DOM elements 
-    // (children of gallerySelector)
-    var parseThumbnailElements = function(el) {
-        var thumbElements = el.childNodes,
-            numNodes = thumbElements.length,
-            items = [],
-            figureEl,
-            linkEl,
-            size,
-            item;
-
-        for(var i = 0; i < numNodes; i++) {
-
-            figureEl = thumbElements[i]; // <figure> element
-
-            // include only element nodes 
-            if(figureEl.nodeType !== 1) {
-                continue;
-            }
-
-            linkEl = figureEl.children[0]; // <a> element
-
-            size = linkEl.getAttribute('data-size').split('x');
-
-            // create slide object
-            item = {
-                src: linkEl.getAttribute('href'),
-                w: parseInt(size[0], 10),
-                h: parseInt(size[1], 10)
-            };
-
-
-
-            if(figureEl.children.length > 1) {
-                // <figcaption> content
-                item.title = figureEl.children[1].innerHTML; 
-            }
-
-            if(linkEl.children.length > 0) {
-                // <img> thumbnail element, retrieving thumbnail url
-                item.msrc = linkEl.children[0].getAttribute('src');
-            } 
-
-            item.el = figureEl; // save link to element for getThumbBoundsFn
-            items.push(item);
-        }
-
-        return items;
-    };
-
-    // find nearest parent element
-    var closest = function closest(el, fn) {
-        return el && ( fn(el) ? el : closest(el.parentNode, fn) );
-    };
-
-    // triggers when user clicks on thumbnail
-    var onThumbnailsClick = function(e) {
-        e = e || window.event;
-        e.preventDefault ? e.preventDefault() : e.returnValue = false;
-
-        var eTarget = e.target || e.srcElement;
-
-        // find root element of slide
-        var clickedListItem = closest(eTarget, function(el) {
-            return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
-        });
-
-        if(!clickedListItem) {
-            return;
-        }
-
-        // find index of clicked item by looping through all child nodes
-        // alternatively, you may define index via data- attribute
-        var clickedGallery = clickedListItem.parentNode,
-            childNodes = clickedListItem.parentNode.childNodes,
-            numChildNodes = childNodes.length,
-            nodeIndex = 0,
-            index;
-
-        for (var i = 0; i < numChildNodes; i++) {
-            if(childNodes[i].nodeType !== 1) { 
-                continue; 
-            }
-
-            if(childNodes[i] === clickedListItem) {
-                index = nodeIndex;
-                break;
-            }
-            nodeIndex++;
-        }
-
-
-
-        if(index >= 0) {
-            // open PhotoSwipe if valid index found
-            openPhotoSwipe( index, clickedGallery );
-        }
-        return false;
-    };
-
-    // parse picture index and gallery index from URL (#&pid=1&gid=2)
-    var photoswipeParseHash = function() {
-        var hash = window.location.hash.substring(1),
-        params = {};
-
-        if(hash.length < 5) {
-            return params;
-        }
-
-        var vars = hash.split('&');
-        for (var i = 0; i < vars.length; i++) {
-            if(!vars[i]) {
-                continue;
-            }
-            var pair = vars[i].split('=');  
-            if(pair.length < 2) {
-                continue;
-            }           
-            params[pair[0]] = pair[1];
-        }
-
-        if(params.gid) {
-            params.gid = parseInt(params.gid, 10);
-        }
-
-        return params;
-    };
-
-    var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
-        var pswpElement = document.querySelectorAll('.pswp')[0],
-            gallery,
-            options,
-            items;
-
-        items = parseThumbnailElements(galleryElement);
-
-        // define options (if needed)
-        options = {
-
-            // define gallery index (for URL)
-            galleryUID: galleryElement.getAttribute('data-pswp-uid'),
-            showHideOpacity: true,
-            bgOpacity: 1,
-            // getThumbBoundsFn: function(index) {
-            //     // See Options -> getThumbBoundsFn section of documentation for more info
-            //     var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
-            //         pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
-            //         rect = thumbnail.getBoundingClientRect(); 
-
-            //     return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
-            // }
-
-        };
-
-        // PhotoSwipe opened from URL
-        if(fromURL) {
-            if(options.galleryPIDs) {
-                // parse real index when custom PIDs are used 
-                // http://photoswipe.com/documentation/faq.html#custom-pid-in-url
-                for(var j = 0; j < items.length; j++) {
-                    if(items[j].pid == index) {
-                        options.index = j;
-                        break;
-                    }
-                }
+$('a[href*="#"]')
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+    if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+    ) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+            scrollTop: target.offset().top
+            }, 500, function() {
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+                return false;
             } else {
-                // in URL indexes start from 1
-                options.index = parseInt(index, 10) - 1;
+                $target.attr('tabindex','-1'); 
+                $target.focus(); 
+            };
+            });
+        }
+    }
+});
+$(document).ready(function() {
+    $('.actors_slider').slick({
+        focusOnSelect: false,
+        slidesToShow: 4,
+        rows: 2,
+        infinite: false,
+        arrows: true,
+        prevArrow: '.actors-prev',
+        nextArrow: '.actors-next',
+        dots: true,
+        responsive: [{
+
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
             }
-        } else {
-            options.index = parseInt(index, 10);
-        }
 
-        // exit if index not found
-        if( isNaN(options.index) ) {
-            return;
-        }
+            }, {
 
-        if(disableAnimation) {
-            options.showAnimationDuration = 0;
-        }
+            breakpoint: 993,
+            settings: {
+                slidesToShow: 2,
+                
+            }
 
-        // Pass data to PhotoSwipe and initialize it
-        gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-        gallery.init();
-    };
+            }, {
 
-    // loop through all gallery elements and bind events
-    var galleryElements = document.querySelectorAll( gallerySelector );
+            breakpoint: 300,
+            settings: {
+                slidesToShow: 2,
+            }
 
-    for(var i = 0, l = galleryElements.length; i < l; i++) {
-        galleryElements[i].setAttribute('data-pswp-uid', i+1);
-        galleryElements[i].onclick = onThumbnailsClick;
+        }]
+    });
+    $('.repertoire__cards').slick({
+        focusOnSelect: false,
+        slidesToShow: 2,
+        infinite: false,
+        arrows: true,
+        prevArrow: '.repertoire-prev',
+        nextArrow: '.repertoire-next',
+        dots: true,
+        responsive: [{
+            breakpoint: 769,
+                settings: {
+                    slidesToShow: 1,
+                }
+
+            }]
+    });
+});
+$(window).resize(function() {
+    if (window.innerWidth <= 767 && ($(".nav").css("display") == "flex") && $(".nav").attr("dmt-menu-status") != "active") {
+        $(".nav").css("display", "none");
     }
-
-    // Parse URL and open gallery if it contains #&pid=3&gid=1
-    var hashData = photoswipeParseHash();
-    if(hashData.pid && hashData.gid) {
-        openPhotoSwipe( hashData.pid ,  galleryElements[ hashData.gid - 1 ], true, true );
+    else if (window.innerWidth > 767 && ($(".nav").css("display") == "flex") && $(".nav").attr("dmt-menu-status") == "active") {
+        $(".nav").attr({"dmt-menu-status": "hidden"});
+        $(".nav__close-btn").fadeOut(0);
+        $(document.body).css({"overflow": "auto", "height": "auto"});
     }
-};
-
-// execute above function
-initPhotoSwipeFromDOM('.repertoire__gallery');
+    else if (window.innerWidth > 767 && ($(".nav").css("display") == "none") && ($(".nav").attr("dmt-menu-status") == "hidden")) {
+        $(".nav").css("display", "flex");
+    }
+});
+// Открытие полноэкранного меню на устройствах с шириной меньше 767px
+$(".menu-icon").click(function(e){
+    if (window.innerWidth <= 767) {
+        e.preventDefault();
+        $(".nav").attr({"dmt-menu-status": "active"}).css("display", "flex").hide().fadeIn();
+        $(".nav__close-btn").fadeIn();
+        $(document.body).css({"overflow": "hidden", "height": "100%"});
+    }
+});
+$(".nav__close-btn, .nav__link").click(function(e){
+    if (window.innerWidth <= 767) {
+        e.preventDefault();
+        $(".nav").attr({"dmt-menu-status": "hidden"}).fadeOut();
+        $(".nav__close-btn").fadeOut();
+        $(document.body).css({"overflow": "auto", "height": "auto"});
+    }
+});
+function validate() {
+    setTimeout(function() {
+      $( "#subscribe" ).removeClass( "btn-subscribe" );
+      $( "#subscribe" ).addClass( "btn-success", 450, callback );
+    }, 2250 );
+  }
+function callback() {
+    setTimeout(function() {
+        $("#subscribe").removeClass( "btn-success" );
+        $("#subscribe").addClass( "btn-subscribe");
+    }, 1250 );
+}
+$(document).on('submit', '#mailing__form', function(e) {
+    e.preventDefault();
+    $( "#subscribe" ).prop( "disabled", true );
+    $( "#subscribe" ).empty().append('<i class="fas fa-circle-notch fa-spin"></i>');
+    $.ajax({
+        type:'POST',
+        url: $('#mailing__form').attr('data-url'),
+        data: {
+            name: $('#mailing__form-name').val(),
+            email: $('#mailing__form-email').val(),
+            csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
+            action: 'post'
+        },
+        success: function(json){
+            if (json.success) {
+                document.getElementById("mailing__form").reset();
+                $('.mailing__form-errors').empty();
+                $( "#subscribe" ).empty().toggleClass(["btn-subscribe", "btn-success"]);
+                setTimeout(function() {
+                    $( "#subscribe" ).toggleClass(["btn-subscribe", "btn-success"]).append('подписаться');
+                }, 1500 );
+            }
+            else {
+            }
+        },
+        error : function(xhr,errmsg,err) {
+            $('.mailing__form-errors').empty();
+            var json = $.parseJSON(xhr.responseText);
+            var fields_name = ['name', 'email'];
+            for (var field in json.form_errors) {   
+                for (var j = 0; j < json.form_errors[field].length; j++) {
+                    $('#mailing__form-' + field + '__errors').append('<li>' + json.form_errors[field][j].message + '</li>');
+                    $( "#subscribe" ).empty().append('подписаться');
+                };
+            };
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+        },
+    });
+    $( "#subscribe" ).prop( "disabled", false );
+});
+$(window).on('load', function() {
+    $('.repertoire__description').each(function(){
+        if ($(this).get(0).scrollHeight != $(this).get(0).clientHeight) {
+            $(this).after('<a class="more t-white" href="">читать далее</a>')
+        }
+    });
+    $('.more').click(function(e) {
+        e.preventDefault();
+        var description = $(this).parent().children('.repertoire__description');
+        if ($(this).text() != 'скрыть') {
+                description.animate({height: description.get(0).scrollHeight});
+                $(this).text("скрыть");
+        }
+        else {
+            description.animate({height: "63px"});
+            $(this).text("читать далее");
+        }
+    })
+});
